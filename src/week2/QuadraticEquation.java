3a01df3014f413d1f35fae59bb45aaf8fcaaa153
boaz ben-moshe <benmo@g.ariel.ac.il>
2022-10-22 16:00:49 +0300

    Minor additional functionality was added (to the case when there are no real solutions).

--- src/week2/QuadraticEquation.java
+++ src/week2/QuadraticEquation.java
@@ -1,5 +1,11 @@
 package week2;
 import java.util.Scanner;
+/**
+ * A very simple example of solving a Quadratic Equation of the form:
+ * x^2+bx+c=0 (note that for simplicity we have assumed that a=1).
+ * @author boaz.ben-moshe
+ *
+ */
 public class QuadraticEquation {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -8,13 +14,18 @@ public class QuadraticEquation {
 		double b = scanner.nextDouble();
 		System.out.print("Please insert c: ");
 		double c = scanner.nextDouble();
-		
+
 		double disciminant = b*b - 4.0*c;
-		double d = Math.sqrt(disciminant);
-		double root1 = (-b + d) / 2.0;
-		double root2 = (-b - d) / 2.0;
 		System.out.println("x^2 +"+b+"x+"+c+" = 0");
-		System.out.println("first solution: "+root1);
-		System.out.println("second solution: "+root2);
+		if(disciminant>=0) {
+			double d = Math.sqrt(disciminant);
+			double root1 = (-b + d) / 2.0;
+			double root2 = (-b - d) / 2.0;
+			System.out.println("first solution: "+root1);
+			System.out.println("second solution: "+root2);
+		}
+		else {
+			System.out.println("There is no (real) solution, as the disciminant is negative ("+disciminant+")");
+		}
 	}
 }
